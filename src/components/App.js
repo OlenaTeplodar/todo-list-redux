268d9ac8a94f1c3bc51d2caf40c86bfe64b7e32a
olenaTeplodar <lena.golubenkova@gmail.com>
2023-03-04 19:25:53 +0200

    add files

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { AppBar } from "./AppBar/AppBar";
 import { TaskForm } from "./TaskForm/TaskForm";
 import { TaskList } from "./TaskList/TaskList";
 
+
 export const App = () => {
   return (
     <Layout>
